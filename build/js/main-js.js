4709d70478900674ba520ae196bea2684106e624
MishaChernov <80amulet63@gmail.com>
2017-10-10 10:48:42 +0300

    Page first V4

--- build/js/main-js.js
+++ build/js/main-js.js
@@ -6,3 +6,21 @@ $('.burger').click(function() {
   $(this).toggleClass('burger--close  burger--open');
   $('.main-menu__list').slideToggle();
 });
+
+$(function functionName() {
+  var header = $(".page-header");
+    $(window).scroll(function() {
+
+      var ser = $(".search");
+      var headerHeight = header.height();
+      var searchHeight = ser.height();
+      var some = window.scrollY;
+
+      if (some >= headerHeight - searchHeight) {
+        header.addClass("fixed");
+      }
+      else {
+        header.removeClass("fixed");
+      }
+  });
+})
